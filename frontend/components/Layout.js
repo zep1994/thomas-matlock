c4bd644a7127a0eb97e9dfe1d652040dd4baf224
zep1994 <thomasmatlockbba@gmail.com>
2022-11-11 14:52:44 -0600

    Update Layout

---
+++ frontend/components/Layout.js
@@ -0,0 +1,18 @@
+import Header from'./Header'
+import styles from'../styles/Layout.module.css'
+import Footer from'./Footer'
+
+const Layout = ({children}) => {
+    return (
+        <>
+            <div className={styles.container}>
+                <Header />
+                    <main className={styles.main }>
+                        {children}
+                    </main>
+                <Footer />
+            </div>
+        </>    
+    )
+}
+export default Layout
