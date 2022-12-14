979e1544297d703f0a81c0172739de37f23ab0f6
zep1994 <thomasmatlockbba@gmail.com>
2022-12-14 14:49:11 -0600

    Add Resume

---
+++ frontend/pages/resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const resume = () => {
+  return (
+    <div>
+        <iframe src="/resume.pdf#toolbar=0" width="100%" height="2200px" />
+    </div>
+  )
+}
+
+export default resume
