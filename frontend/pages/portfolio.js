bde3dffe3ba8243868c301176d1a23d6f724d19f
zep1994 <thomasmatlockbba@gmail.com>
2022-12-14 11:30:25 -0600

    Update Portfolio

--- frontend/pages/portfolio.js
+++ frontend/pages/portfolio.js
@@ -4,29 +4,41 @@ import styles from '../styles/Portfolio.module.css'
 const portfolio = () => {
     return (
     <div className={styles.container}>
-        <main className={styles.main}>
         <h1 className={styles.title}>
             My Portfolio
         </h1>
-            <br />
-            <img src="/images/stewpot.png" className={styles.stewpot} />
-            <br />
-            <h3>Stewpot</h3>
-            <img src="/images/cline.png" className={styles.cline} />
-            <br />
-            <img src="/images/cline_form.png" className={styles.cline} />
-            <br />
-            <h3>Cline Tours</h3>
-            <img src="/images/mssm.png" className={styles.mssm} />
-            <br />
-            <img src="/images/mssmhome.png" className={styles.mssm} />
-            <br />
-            <h3>Mississippi Sport Medicine</h3>
-            <br />
-            <img src="/images/nucor.png" className={styles.nucor} />
-            <br />
-            <h3>Nucor General Recycling</h3>
-        </main>
+        <div className={styles.sites}>
+            <div className={styles.card}>
+                <img src="/images/stewpot.png" className={styles.stewpot} />
+                <br />
+                <h3>Stewpot</h3>
+                <p>Nextjs, Strapi, SQL, Node, HTML/CSS</p>
+            </div>
+            <div className={styles.card}>
+                <img src="/images/cline.png" className={styles.cline} />
+                <br />
+                <img src="/images/cline_form.png" className={styles.cline} />
+                <br />
+                <h3>Cline Tours</h3>
+                <p>Nextjs, Strapi, SQL, Node, HTML/CSS</p>
+            </div>
+            <div className={styles.card}>
+                <img src="/images/mssm.png" className={styles.mssm} />
+                <br />
+                <img src="/images/mssmhome.png" className={styles.mssm} />
+                <br />
+                <h3>Mississippi Sport Medicine</h3>
+                <p>Laravel, jQuery, SQL, HTML/CSS</p>
+            </div>
+            <div className={styles.card}>
+                <br />
+                <img src="/images/nucor.png" className={styles.nucor} />
+                <br />
+                <h3>Nucor General Recycling</h3>
+                <p>Laravel, jQuery, SQL, HTML/CSS</p>
+
+            </div>
+        </div>
     </div>
     )
 }
