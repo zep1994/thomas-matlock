b8e7dbd8fa5c85764ca41c25b0b6aec975fb7d4c
zep1994 <thomasmatlockbba@gmail.com>
2022-12-20 13:40:39 -0600

    Update Site

---
+++ frontend/pages/skills.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/Skills.module.css'
+
+const skills = () => {
+  return (
+        <div className={styles.mainContainer}>
+            <p className={styles.content}>
+                React, NextJS,, Laravel, HTML/CSS, Node, Python, Ruby, SQL
+            </p>
+            <br />
+        </div>
+    )
+}
+
+export default skills
