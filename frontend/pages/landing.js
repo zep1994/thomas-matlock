486bdd6bc1a5f8a899eca1cf9d86c28738b6bacb
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-01 11:45:52 -0600

    add Landing page

---
+++ frontend/pages/landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../styles/Landing.module.css'
+
+const landing = () => {
+  return (
+    <div className={styles.mainContent}>
+        <div className={styles.contentLeft}>
+            <div className={styles.circle}>
+                
+            </div>
+        </div>
+        <div className={styles.contentRight}>
+
+        </div>
+    </div>
+  )
+}
+
+export default landing
