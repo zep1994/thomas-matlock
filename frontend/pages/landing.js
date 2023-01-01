ddd22ce438144bb77c6f5fd440bf785023c4295e
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-01 14:12:07 -0600

    add text animation

--- frontend/pages/landing.js
+++ frontend/pages/landing.js
@@ -7,6 +7,14 @@ const landing = () => {
             <div className={styles.content}>
                 {/* SECTION 1 */}
                 <div className={styles.contentLeft}>
+                <h1 className={styles.h1}>
+                    <span>always be</span>
+                    <div className={styles.message}>
+                        <div className={styles.word1}>close</div>
+                        <div className={styles.word2}>undeniable</div>
+                        <div className={styles.word3}>creating</div>
+                    </div>
+                </h1>
                     <div className={styles.img}>
                         
                     </div>
@@ -20,9 +28,9 @@ const landing = () => {
                         <p><strong>DEVELOPER</strong></p>
                     </div>
                     <svg className={styles.arrows}>
-                        <path class="a1" d="M0 0 L30 32 L60 0"></path>
-                        <path class="a2" d="M0 20 L30 52 L60 20"></path>
-                        <path class="a3" d="M0 40 L30 72 L60 40"></path>
+                        <path className={styles.a1} d="M0 0 L30 32 L60 0"></path>
+                        <path className={styles.a2} d="M0 20 L30 52 L60 20"></path>
+                        <path className={styles.a3} d="M0 40 L30 72 L60 40"></path>
                     </svg>
                 </div>
             </div>
