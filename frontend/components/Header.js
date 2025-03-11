f47a024671d1ca5ff898727c1fb9bab9d3c6d08f
Thomas Matlock <thomasmatlockbba@gmail.com>
2025-03-11 16:27:22 -0500

    Update Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,57 +1,43 @@
 import React from "react";
-import styles from "../styles/Header.module.css";
+import Link from "next/link";
+import styles from "../styles/Home.module.css"; // Using Home.module.css for consistent styling
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      {/* <div className={styles.nav}>
-        <ul className={styles.navUl}>
-          <div className={styles.menuUl}>
-            <div className={styles.navPrimaryHeader}>
-              <img src="/images/me2.jpg" alt="" className={styles.headerImg_nav} />
-              <h1>THOMAS MATLOCK</h1>
-            </div>
-            <a href="/">
-              <li className={styles.nav_MenuItems}>HOME</li>
-            </a>
-            <a href="/about">
-              <li className={styles.nav_MenuItems}>ABOUT</li>
-            </a>
-            <a href="/portfolio">
-              <li className={styles.nav_MenuItems}>PORTFOLIO</li>
-            </a>
-            <a href="/resume">
-              <li className={styles.nav_MenuItems}>RESUME</li>
-            </a>
-            <a href="/skills">
-              <li className={styles.nav_MenuItems}>SKILLS</li>
-            </a>
-            <a href="/contact">
-              <li className={styles.nav_MenuItems}>CONTACT</li>
-            </a>
-            <a href="/contact">
-              <button type="button" className={styles.navContactBtn}>Call Me</button>
-            </a>
-            <p className={styles.nav_p}>
-              Thomas Matlock <br />
-              Jackson, MS, 39211 <br />
-              United States
-            </p>
-          </div>
-        </ul>
+    <header className={styles.navigation}>
+      <div className={styles.logoTran}>
+        <Link href="/">
+            <img src="/images/logo.png" className={styles.logo} alt="logo" />
+            <p className={styles.name}>homas</p>
+        </Link>
       </div>
-      <div className={styles.top}>
-      <div className={styles.shape}></div>
-        <div className={styles.topLeft}>
-          <h1 className={styles.primaryTitle}>CLAIRVOYANT</h1>
-          <br />
-          <h1 className={styles.secondaryTitle}>FULL STACK DEVELOPER</h1>
-        </div>
-        <div className={styles.topRight}>
-          <img src='/images/tags.png' alt='Coding Tags' className={styles.codingImg} />
-        </div>
-        </div> */}
-    </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <i className={styles.locator}>.01</i>
+          <Link href="/#about">About</Link>
+        </li>
+        <li>
+          <i className={styles.locator}>.02</i>
+          <Link href="/#experience">About</Link>
+
+        </li>
+        <li>
+          <i className={styles.locator}>.03</i>
+          <Link href="/#work">About</Link>
+        </li>
+        <li>
+          <i className={styles.locator}>.04</i>
+          <Link href="/#contact">Contact</Link>
+        </li>
+        <li>
+          <button>
+            <a href="/Thomas Matlock Resume.pdf" target="_blank">
+              RESUME
+            </a>
+          </button>
+        </li>
+      </ul>
+    </header>
   );
 };
 
